6599786d440f274678001f396b3f475b7e11771b
Franck Michea <franck.michea@gmail.com>
2013-03-05 08:35:18 +0100

    Update test for ByteArrayField.

--- tests/foo.py
+++ tests/foo.py
@@ -5,13 +5,14 @@ class B(sm.Struct):
     b_first = sf.Field(size=sf.Field_Sizes.INT32)
 
 class A(sm.Struct):
-    a_first = sf.Field()
-    a_second = sf.Field(size=sf.Field_Sizes.INT32,
+    a_first = sf.Field('First field')
+    a_second = sf.Field('Second field', size=sf.Field_Sizes.INT32,
                         endianess=sf.Field_Endianess.BIG)
     a_third = sf.SuperField(B)
     a_fourth = sf.Array(4, sf.Field())
+    a_fifth = sf.ByteArrayField('Fifth field', size=10)
 
-data = bytearray(bytes.fromhex('2adeadbeefefbeadde42434445'))
+data = bytearray(bytes.fromhex('2adeadbeefefbeadde4243444500010203040506070809'))
 
 a = A(data, 0)
 
@@ -21,6 +22,8 @@ assert(a.a_third.b_first == 0xdeadbeef)
 
 for i in range(4):
     assert(a.a_fourth[i] == (0x42 + i))
+
+assert(a.a_fifth == bytearray.fromhex('00010203040506070809'))
 
 print('data:', data)
 a.a_first = 0x01
