15dae3f0d2881dd9ae0c8cd0f73a2979eae8ce53
Franck Michea <franck.michea@gmail.com>
2013-03-05 09:14:13 +0100

    Implementation of padding fields.

--- srddl/fields/padding.py
+++ srddl/fields/padding.py
@@ -8,4 +8,20 @@ from srddl.core.abstract import AbstractField
 
 PaddingMode = sch.enum(TAKE=0, FILL=1)
 
-class Padding(AbstractField): pass
+class Padding(AbstractField):
+    def __init__(self, size, **kwargs):
+        self._size, self._mode = size, kwargs.get('mode', PaddingMode.TAKE)
+
+    def __get__(self, instance, owner=None):
+        return None
+
+    def __set__(self, instance, value):
+        pass
+
+    def size(self, instance):
+        if self._mode == PaddingMode.TAKE:
+            return self._size
+        elif self._mode == PaddingMode.FILL:
+            res = self._size - self.offset(instance)
+            return res if 0 <= res else 0
+        return None
