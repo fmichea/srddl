1817b3efae2715ed013c2339acd5760a11a56830
Franck Michea <franck.michea@gmail.com>
2013-03-05 07:18:09 +0100

    Add main file with main function.

---
+++ srddl/main.py
@@ -0,0 +1,11 @@
+# srddl/main.py - Main file, entry point of the program.
+# Author: Franck Michea <franck.michea@gmail.com>
+# License: New BSD License (See LICENSE)
+
+import sys
+
+def main(args):
+    pass
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
