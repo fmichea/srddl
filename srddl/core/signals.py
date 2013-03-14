5afb88cb62494a6fdfa1266bb096efc8049af5f5
Franck Michea <franck.michea@gmail.com>
2013-03-14 13:06:50 +0100

    Add signal implementation, tests will follow.

---
+++ srddl/core/signals.py
@@ -0,0 +1,78 @@
+# srddl/core/signal.py - Signal management stuff.
+# Author: Franck Michea <franck.michea@gmail.com>
+# License: New BSD License (See LICENSE)
+
+import srddl.exceptions as se
+
+class _SignalHandler:
+    def __init__(self, func, kwargs):
+        self._func, self._kwargs = func, kwargs
+
+    def __repr__(self):
+        return 'SignalHandler {:#x} on {}'.format(id(self), self._func)
+
+    def __call__(self, args):
+        self._func(*args, **self._kwargs)
+
+
+class Signal:
+    def __init__(self):
+        self._signals = dict()
+
+    def create(self, signame):
+        '''
+        :param signame: Name to give to the signal, it will be its identifier.
+
+        Creates a signal with the given signal name. This is mandatory because
+        we will use strings for signal names and we don't want to subscribe
+        to a signal that doesn't exist by mistake because of a typo.
+        '''
+        if signame in self._signals:
+            raise se.SignalExistsError(signame)
+        self._signals[signame] = dict()
+
+    def subscribe(self, signame, func, **kwargs):
+        '''
+        :param signame: The name of the signal.
+        :param func: The callback to call on signal trigger.
+        :return: An id identifying the handler, used by unsubscribe.
+
+        This function permits to subscribe to some signal. You must give a
+        callback function that will be called on signal triggering.  You can
+        add some keyword parameters that will also be given to the function, so
+        that you can have "closures".
+        '''
+        if signame not in self._signals:
+            raise se.SignalNotFoundError(signame, 'subscribing')
+        sh = _SignalHandler(func, args, kwargs)
+        self._signals[signame][id(sh)].append(sh)
+        return id(sh)
+
+    def unsubscribe(self, signame, idx):
+        '''
+        :param signame: The name identifying the signal.
+        :param idx: The id returned by subscribe method.
+
+        You can unsubscribe to a signal using this method. After this, the
+        handler function won't be called again.
+        '''
+        if signame not in self._signals:
+            raise se.SignalNotFoundError(signame, 'unsubscribing')
+        if idx not in self._signals[signame]:
+            raise se.SignalHandlerNotFoundError(signame, idx)
+        del self._signals[signame][id(sh)]
+
+    def trigger(self, signame, *args):
+        '''
+        With this function you can trigger a signal.
+
+        :param signame: The signal name to trigger.
+        :param args: Arguments to give to the signal.
+        '''
+        if signame not in self._signals:
+            raise se.SignalNotFoundError(signame, 'triggering')
+        for func in self._signals[signame].values():
+            try:
+                func(args)
+            except TypeError:
+                raise se.SignalHandlerCallFail(signame, func)
--- srddl/exceptions.py
+++ srddl/exceptions.py
@@ -72,3 +72,44 @@ class NotABoundValueError(Exception):
     def __str__(self):
         res = 'The value returned by {field} was not a BoundValue.'
         return res.format(field=self.field)
+
+
+# ----- Signal Exceptions ------------------------------------------------------
+
+class SignalExistsError(Exception):
+    def __init__(self, signame):
+        self.signame = signame
+
+    def __str__(self):
+        return 'Tried to create signal "{signame}" that already exists.'.format(
+            self.signame
+        )
+
+
+class SignalNotFoundError(Exception):
+    def __init__(self, signame, action):
+        self.signame, self.action = signame, action
+
+    def __str__(self):
+        return 'The signal "{signame}" was not found while {action}.'.format(
+            signame=self.signame, action=self.action
+        )
+
+
+class SignalHandlerNotFoundError(Exception):
+    def __init__(self, signame, idx):
+        self.signame, self.idx = signame, idx
+
+    def __str__(self):
+        res = 'Signal handler {idx:#x} was not found for signal "{signame}".'
+        return res.format(signame=self.signame, idx=self.idx)
+
+
+class SignalHandlerCallError(Exception):
+    def __init__(self, signame, func):
+        self.signame, self.func = signame, func
+
+    def __str__(self):
+        res = 'Signal handler {func} call was not successful for signal '
+        res += '"{signame}". Check backtrace.'
+        return res.format(func=self.func, signame=self.signame)
