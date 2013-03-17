c6e24ca18989fc11f9bed9f8c8e7aee97071e138
Franck Michea <franck.michea@gmail.com>
2013-03-17 15:36:35 +0100

    Update simple_dependency example to show power of reference management.

--- examples/simple_dependency.py
+++ examples/simple_dependency.py
@@ -1,11 +1,17 @@
 import srddl.models as sm
 import srddl.fields as sf
 
+buf = bytearray.fromhex('0242434445')
+
 class Foo(sm.Struct):
     length = sf.IntField()
-    data = sf.Array(length, sf.Array(length, sf.IntField()))
+    data = sf.ArrayField(length, sf.IntField())
 
-f = Foo(bytearray.fromhex('0242434445'), 0)
+f = Foo(buf, 0)
 
-#assert(f.length == 2)
-#assert(f.data == [0x42, 0x43]))
+assert(f.length == 2)
+assert(f.data == [0x42, 0x43])
+
+f.length = 4
+
+assert(f.data == [0x42, 0x43, 0x44, 0x45])
