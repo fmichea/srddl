f27ee7fe172e851c7c8d3ad1262326f4b091a5b1
Franck Michea <franck.michea@gmail.com>
2013-03-21 19:11:14 +0100

    Add offset module to express offsets and sizes.

---
+++ srddl/core/offset.py
@@ -0,0 +1,53 @@
+# srddl.core.offset - Helps describe offsets and sizes.
+# Author: Franck Michea <franck.michea@gmail.com>
+# License: New BSD License (See LICENSE)
+
+import functools
+
+@functools.total_ordering
+class _Offset:
+    def __init__(self, byte=0, bit=0):
+        if isinstance(byte, _Offset):
+            self.byte, self.bit = byte.byte, byte.bit
+        else:
+            self.byte, self.bit = byte, bit
+
+    def __eq__(self, other):
+        # This function is needed by functools.total_ordering.
+        if isinstance(other, _Offset):
+            return (self.byte, self.bit) == (other.byte, other.bit)
+        return (self.byte, self.bit) == (other, 0)
+
+    def __lt__(self, other):
+        # This function is needed by functools.total_ordering.
+        if isinstance(other, _Offset):
+            return (self.byte, self.bit) < (other.byte, other.bit)
+        return (self.byte, self.bit) < (other, 0)
+
+    def __add__(self, other):
+        if isinstance(other, _Offset):
+            bit = self.bit + other.bit
+            byte = self.byte + other.byte + (bit >> 3)
+            bit &= 0b111
+            return self.__class__(byte=byte, bit=bit)
+        return self.__class__(byte=(self.byte + other), bit=self.bit)
+
+    def __sub__(self, other):
+        if isinstance(other, _Offset):
+            bit = self.bit - other.bit
+            byte = self.byte + other.byte
+            if bit < 0:
+                bit = 8 - bit
+                byte -= 1
+            return self.__class__(byte=byte, bit=bit)
+        return self.__class__(byte=(self.byte - other), bit=self.bit)
+
+    def __rsub__(self, other):
+        return self.__class__(byte=(other - self.byte), bit=self.bit)
+
+    def aligned(self):
+        return (self.bit == 0)
+
+
+class Offset(_Offset): pass
+class Size(_Offset): pass
