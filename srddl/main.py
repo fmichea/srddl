560129b84696a9936967df762f244089a8e27455
Franck Michea <franck.michea@gmail.com>
2013-04-04 14:47:39 +0200

    Add begin of command line parsing.

--- srddl/main.py
+++ srddl/main.py
@@ -2,7 +2,35 @@
 # Author: Franck Michea <franck.michea@gmail.com>
 # License: New BSD License (See LICENSE)
 
+import argparse
+import pdb
 import sys
+import traceback
+
 
 def main():
-    print('See you soon. ;)')
+    parser = argparse.ArgumentParser(description='Templated hexadecimal editor.')
+
+    dgroup = parser.add_argument_group(title='srddl debugging features')
+    dgroup.add_argument('-b', '--backtrace', action='store_true', default=False,
+                        help='print backtrace on error (default with --pdb).')
+    dgroup.add_argument('-P', '--pdb', action='store_true', default=False,
+                        help='start pdb debugger on exception.')
+
+    args = parser.parse_args(sys.argv[1:])
+    if hasattr(args, 'func'):
+        try:
+            args.func(sys.argv, args)
+        except Exception as err:
+            if args.backtrace or args.pdb:
+                traceback.print_exc()
+            if args.pdb:
+                pdb.post_mortem(sys.exc_info()[2])
+            elif not args.backtrace:
+                header, output = 'ERROR: ', []
+                for txt in str(err).splitlines():
+                    output.append(' ' * len(header) + txt)
+                output = header + '\n'.join(output)[len(header):]
+                sys.exit(output)
+    else:
+        parser.print_help()
