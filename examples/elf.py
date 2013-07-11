91fe3811a0ed906a59c11762c47078beb86f9f44
Franck Michea <franck.michea@gmail.com>
2013-07-11 19:12:53 +0200

    Open RDWR the given filename instead of /bin/ls in ELF example.

--- examples/elf.py
+++ examples/elf.py
@@ -10,7 +10,12 @@ if 1 < len(sys.argv) and sys.argv[1] == 'benchmark':
     COUNT = 10
 begin, d, e = time.perf_counter(), None, None
 for _ in range(COUNT):
-    d = sd.FileData('/bin/ls')
+    try:
+        filename = sys.argv[1]
+    except IndexError:
+        filename = '/bin/ls'
+    d = sd.FileData(filename, mode=sd.FileData.Mode.RDWR)
+
     e = ELF()
     e.setup(d)
 elapsed = (time.perf_counter() - begin) / COUNT
