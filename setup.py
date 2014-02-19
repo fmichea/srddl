4ec395f3db73ac2a9182badf2b34563b92482a2b
Franck Michea <franck.michea@gmail.com>
2014-02-19 01:25:19 +0100

    Enhance pytest integration and setup a correct virtualenv etc.

--- setup.py
+++ setup.py
@@ -1,16 +1,27 @@
 import sys
 try:
     from setuptools import setup, find_packages
+    from setuptools.command.test import test as TestCommand
 except ImportError:
     import distribute_setup
     distribute_setup.use_setuptools()
     from setuptools import setup, find_packages
+    from setuptools.command.test import test as TestCommand
 
 if sys.version_info[0] != 3:
     msg = 'srddl is intended to be used with python 3 only (it uses specific '
     msg += 'features).\nMinimum version is not known yet, but this code was '
     msg += 'tested for python 3.3 at least (released September 29, 2012).'
     sys.exit(msg)
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        super().finalize_options()
+        self.test_args, self.test_suite = ['tests'], True
+
+    def run_tests(self):
+        import pytest
+        sys.exit(pytest.main(self.test_args))
 
 setup(
     # General information.
@@ -30,6 +41,10 @@ setup(
     install_requires=open('requirements.txt').readlines(),
     packages=find_packages(exclude=['tests', 'examples', 'doc']),
     entry_points = {'console_scripts': ['srddl = srddl.main:main']},
+
+    # Test integration (pytest).
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
 
     # PyPI categories.
     classifiers=[
